680dafd6824b36d086e9fc9d0c124a61200a1547
Brandon Carter <brcarter1@student.fullsail.edu>
2026-07-01 01:26:57 -0500

    added faker js

---
+++ src/js/data/user.js
@@ -0,0 +1,17 @@
+import { faker } from "@faker-js/faker";
+
+const avatar = faker.image.avatar();
+const person = faker.person.firstName();
+const email = faker.internet.email();
+const latitude = faker.location.latitude();
+const longitude = faker.location.longitude();
+
+const userProfile = {
+  avatar,
+  name,
+  email,
+  latitude,
+  longitude,
+};
+
+export default userProfile;
